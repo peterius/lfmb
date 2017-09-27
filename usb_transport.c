cef0611c7ab514575485837dd4b97f3c8ea4b307
Peter Bohning <peter.bohning@gmx.com>
2017-09-27 14:48:26 -0400

    Heimdall copyright notice

--- usb_transport.c
+++ usb_transport.c
@@ -14,6 +14,11 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
+/* ***
+ * HAS_LIBUSB code largely modified from Heimdall 
+ * Copyright (c) 2010-2017 Benjamin Dobell, Glass Echidna
+ * *** */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <fcntl.h>
